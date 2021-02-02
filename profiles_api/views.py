02395abb2c3ed0a0cecc7fbb18ca6fddfde69bee
Sean <pereirasean1811@gmail.com>
2021-02-01 20:48:59 -0800

    Added Login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,6 +9,15 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
